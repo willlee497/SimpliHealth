fb76804d16baae1379a25df449d62dad0e02a2ad
salmjd <salmanmajid1975@gmail.com>
2024-11-09 08:25:06 -0500

    feat: big commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,10 @@
-import Image from "next/image";
-import Header from "@/components/Header";
+import InputBox from "@/components/InputBox";
+import ChatDisplay from "@/components/Chat";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      
+    <div>
+      <ChatDisplay />
     </div>
   );
 }
